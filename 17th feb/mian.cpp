b98b94fe78dc16ee15e54877f14940bf2a2565e9
AHSISH KUAMR SINGH <officialwork.ashish@gmail.com>
2023-02-17 21:29:25 +0530

    17TH FEB COMMEITS

--- 17th feb/mian.cpp
+++ 17th feb/mian.cpp
@@ -1,1 +1,13 @@
-@inline
+#include <iostream>
+using namespace std;
+
+int main(){
+    string first ;
+    string second;
+    cin >> first ;
+    cin >> second;
+    string concatenate;
+    concatenate = first + second ;
+    cout << concatenate ;
+    return 0;
+}
